0511f8a72673d670277b3c4ddf0ba5b58f446c39
tha-bit <52799984+tha-bit@users.noreply.github.com>
2024-01-28 16:38:09 +0000

    CSS + HTML

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-  return "Hello, worldsss"
+def hello_3T():
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
-  print("I am inside if now")
   app.run(host='0.0.0.0', debug=True)
